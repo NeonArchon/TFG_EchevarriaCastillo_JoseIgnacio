211bc5e5f5f10dcb5d433af1d3f30ee20ee06cf1
Jose <mortox1990@gmail.com>
2025-05-05 19:13:54 +0200

    Creando la clase para desarrollar codigo de la pantalla de visualizacion

---
+++ app/src/main/java/com/example/tfg_planeta_maqueta/PantallaTienda.kt
@@ -0,0 +1,14 @@
+package com.example.tfg_planeta_maqueta
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PantallaTienda : AppCompatActivity()  {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalla_tienda)
+
+    }
+
+}
